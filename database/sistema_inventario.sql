203306a79605870f43eabb2c2fa8d029a76af819
camilo hernandez <ch4602117@gmail.com>
2023-10-24 20:05:02 -0500

    database actualizada

--- database/sistema_inventario.sql
+++ database/sistema_inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2023 a las 03:46:42
+-- Tiempo de generación: 25-10-2023 a las 03:03:55
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,78 +24,224 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Estructura de tabla para la tabla `ciudad`
 --
 
-CREATE TABLE `productos` (
-  `id_Producto` int(30) UNSIGNED NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Cantidad` int(30) NOT NULL,
-  `IdUsuarios` int(30) NOT NULL,
-  `Meta` int(30) NOT NULL
+CREATE TABLE `ciudad` (
+  `Id_Ciudad` int(10) UNSIGNED NOT NULL,
+  `Ciudad` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`id_Producto`, `Nombre`, `Cantidad`, `IdUsuarios`, `Meta`) VALUES
-(1, 'Papas Margarita', 3, 1, 1),
-(2, 'Gaseosa', 20, 1, 1),
-(3, 'Papas Margarita', 3, 1, 1),
-(4, 'Gaseosa', 20, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `cliente`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(30) UNSIGNED NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `role` int(1) NOT NULL
+CREATE TABLE `cliente` (
+  `Id_cliente` int(10) UNSIGNED NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellidos` varchar(45) NOT NULL,
+  `Telefono` varchar(12) NOT NULL,
+  `Correo` varchar(45) NOT NULL,
+  `Contraseña` varchar(45) NOT NULL,
+  `Dirección_Id_Dirección` int(10) UNSIGNED NOT NULL,
+  `Fecha_nacimiento` date NOT NULL,
+  `Sexo` enum('hombre','mujer','otro') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `usuario`
+-- Estructura de tabla para la tabla `dirección`
 --
 
-INSERT INTO `usuario` (`id`, `email`, `password`, `role`) VALUES
-(1, 'ch4602117@gmail.com', '1234567', 1);
+CREATE TABLE `dirección` (
+  `Id_Dirección` int(10) UNSIGNED NOT NULL,
+  `Dirección` varchar(45) NOT NULL,
+  `Ciudad_Id_Ciudad` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `Id_empleado` int(10) UNSIGNED NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellidos` varchar(45) NOT NULL,
+  `Correo` varchar(45) NOT NULL,
+  `Contraseña` varchar(45) NOT NULL,
+  `Roles_Id_roles` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `idFactura` int(10) UNSIGNED NOT NULL,
+  `Fecha` date NOT NULL,
+  `Pedido_idPedido` int(10) UNSIGNED NOT NULL,
+  `Total` decimal(10,2) NOT NULL,
+  `Subtotal` decimal(10,2) NOT NULL,
+  `Empleado_Id_empleado` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idPedido` int(10) UNSIGNED NOT NULL,
+  `Cliente_Id_cliente` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `platillo`
+--
+
+CREATE TABLE `platillo` (
+  `idPlatillo` int(10) UNSIGNED NOT NULL,
+  `Nombre_platillo` varchar(45) NOT NULL,
+  `Precio` decimal(10,2) NOT NULL,
+  `Descripción` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `Id_roles` int(10) UNSIGNED NOT NULL,
+  `Descripción` varchar(45) NOT NULL,
+  `Rol` enum('empleado','administrador') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `productos`
+-- Indices de la tabla `ciudad`
 --
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id_Producto`);
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`Id_Ciudad`);
 
 --
--- Indices de la tabla `usuario`
+-- Indices de la tabla `cliente`
 --
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`Id_cliente`),
+  ADD KEY `Dirección_Id_Dirección` (`Dirección_Id_Dirección`);
+
+--
+-- Indices de la tabla `dirección`
+--
+ALTER TABLE `dirección`
+  ADD PRIMARY KEY (`Id_Dirección`),
+  ADD KEY `Ciudad_Id_Ciudad` (`Ciudad_Id_Ciudad`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`Id_empleado`),
+  ADD KEY `Roles_Id_roles` (`Roles_Id_roles`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`idFactura`),
+  ADD KEY `fk_Factura_Pedido1` (`Pedido_idPedido`),
+  ADD KEY `Empleado_Id_empleado` (`Empleado_Id_empleado`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `Cliente_Id_cliente` (`Cliente_Id_cliente`);
+
+--
+-- Indices de la tabla `platillo`
+--
+ALTER TABLE `platillo`
+  ADD PRIMARY KEY (`idPlatillo`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`Id_roles`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `productos`
+-- AUTO_INCREMENT de la tabla `cliente`
 --
-ALTER TABLE `productos`
-  MODIFY `id_Producto` int(30) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `cliente`
+  MODIFY `Id_cliente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `usuario`
+-- AUTO_INCREMENT de la tabla `empleado`
 --
-ALTER TABLE `usuario`
-  MODIFY `id` int(30) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `empleado`
+  MODIFY `Id_empleado` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `Id_roles` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`Dirección_Id_Dirección`) REFERENCES `dirección` (`Id_Dirección`);
+
+--
+-- Filtros para la tabla `dirección`
+--
+ALTER TABLE `dirección`
+  ADD CONSTRAINT `dirección_ibfk_1` FOREIGN KEY (`Ciudad_Id_Ciudad`) REFERENCES `ciudad` (`Id_Ciudad`);
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`Roles_Id_roles`) REFERENCES `roles` (`Id_roles`);
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`Empleado_Id_empleado`) REFERENCES `empleado` (`Id_empleado`),
+  ADD CONSTRAINT `fk_Factura_Pedido1` FOREIGN KEY (`Pedido_idPedido`) REFERENCES `pedido` (`idPedido`);
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`Cliente_Id_cliente`) REFERENCES `cliente` (`Id_cliente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
