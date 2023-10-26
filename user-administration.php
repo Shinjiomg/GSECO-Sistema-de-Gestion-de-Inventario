2699728c0a971c2302f68fedcd989a5948c1fb98
Jhonatan Becerra <yonkitas9@gmail.com>
2023-10-26 09:06:26 -0500

    advances

--- user-administration.php
+++ user-administration.php
@@ -52,7 +52,7 @@ include_once("Consultas.php");
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link ">
+                    <a class="nav-link" href="sales.php">
                         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="ni ni-cart text-primary text-sm opacity-10"></i>
                         </div>
