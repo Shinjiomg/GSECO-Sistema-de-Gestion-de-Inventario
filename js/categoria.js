cc1a32d242855d78177e490d9f487f4de0eba461
Nicollg18 <143122517+Nicollg18@users.noreply.github.com>
2023-11-07 22:47:16 -0500

    avances

--- js/categoria.js
+++ js/categoria.js
@@ -11,7 +11,12 @@ function guardarCategoria(){
         contentType: false,
         processData: false,
         success: function (response) {
-                
+            Swal.fire({
+                title: "Generar categoria",
+                text: "La categoria se guardo correctamente",
+                icon: "success"
+            });
+            $('#modal-form').modal('hide');
         }
     });
 }
