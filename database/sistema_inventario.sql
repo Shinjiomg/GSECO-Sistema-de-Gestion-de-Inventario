8e0a4f14b4fc66f9f2b3a4fb1f156e7f91e1a65e
Alexander <alexander@gmail.com>
2023-12-09 17:16:57 -0500

    triggers implemented

--- database/sistema_inventario.sql
+++ database/sistema_inventario.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-12-2023 a las 00:14:02
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 09-12-2023 a las 23:14:51
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -47,8 +47,8 @@ INSERT INTO `articulo` (`id_articulo`, `nombre`, `precio_venta`, `stock`, `descr
 (2, 'AGUA CRISTAL MINI ', 700.00, 0, NULL, 1, 3, 20),
 (3, 'AGUACRISTAL SIN GAS 600 ML', 1600.00, 0, NULL, 1, 3, 20),
 (4, 'AGUS CRISTAL COCO', 2000.00, 0, NULL, 1, 3, 20),
-(5, 'AREPA DE HUEVO', 3200.00, 0, NULL, 1, 8, 20),
-(6, 'AREPA DE QUESO', 2500.00, 0, NULL, 1, 8, 20),
+(5, 'AREPA DE HUEVO', 3200.00, -13, NULL, 1, 8, 20),
+(6, 'AREPA DE QUESO', 2500.00, -13, NULL, 1, 8, 20),
 (7, 'ARROZ - PORCI?N', 2000.00, 0, NULL, 1, 1, 20),
 (8, 'ARROZ CON LECHE', 3000.00, 0, NULL, 1, 10, 20),
 (9, 'AVENA BOLSA ', 1900.00, 0, NULL, 1, 10, 20),
@@ -95,7 +95,7 @@ INSERT INTO `articulo` (`id_articulo`, `nombre`, `precio_venta`, `stock`, `descr
 (50, 'DONUTS  CARMENZA', 2500.00, 0, NULL, 1, 11, 20),
 (51, 'DORITOS', 2000.00, 0, NULL, 1, 4, 20),
 (52, 'DORITOS DINAMITA', 2200.00, 0, NULL, 1, 4, 20),
-(53, 'EMPANADA PAPA-CARNE', 2200.00, 0, NULL, 1, 8, 20),
+(53, 'EMPANADA PAPA-CARNE', 2200.00, -6, NULL, 1, 8, 20),
 (54, 'FRUTA - PORCI?N', 3000.00, 0, NULL, 1, 1, 20),
 (55, 'GALLETA COCOSETTE', 1700.00, 0, NULL, 1, 7, 20),
 (56, 'GALLETA FESTIVAL', 1500.00, 0, NULL, 1, 7, 20),
@@ -157,12 +157,12 @@ INSERT INTO `articulo` (`id_articulo`, `nombre`, `precio_venta`, `stock`, `descr
 (112, 'PAN ', 1600.00, 0, NULL, 1, 11, 20),
 (113, 'PAN 70 - LIBERAL', 1500.00, 0, NULL, 1, 11, 20),
 (114, 'PAN 70 LIBERAL', 1500.00, 0, NULL, 1, 11, 20),
-(115, 'PAN DE BONO ESTELA', 2200.00, 0, NULL, 1, 8, 20),
+(115, 'PAN DE BONO ESTELA', 2200.00, -1, NULL, 1, 8, 20),
 (116, 'PAN LA GRAN SUIZA', 1600.00, 0, NULL, 1, 11, 20),
 (117, 'PAN PIZZA', 1800.00, 0, NULL, 1, 11, 20),
 (118, 'PANDEBONO', 2300.00, 0, NULL, 1, 8, 20),
 (119, 'PAPA PORCI?N', 3000.00, 0, NULL, 1, 1, 20),
-(120, 'PAPA RELENA', 3000.00, 0, NULL, 1, 8, 20),
+(120, 'PAPA RELENA', 3000.00, -1, NULL, 1, 8, 20),
 (121, 'PAPAS MARGARITAS', 2000.00, 0, NULL, 1, 4, 20),
 (122, 'PAPEL HIGIENICO ', 2500.00, 0, NULL, 1, 2, 20),
 (123, 'PASTECLO', 1500.00, 0, NULL, 1, 11, 20),
@@ -248,8 +248,9 @@ INSERT INTO `categoria_gastos` (`id`, `descripcion`) VALUES
 (6, 'Elementos de aseo y cafetería'),
 (7, 'Utiles, papeleria y fotocopías'),
 (8, 'Taxis y buses'),
-(9, 'Diversos');
-
+(9, 'Diversos'),
+(10, 'test'),
+(11, 'no_operacionales');
 
 -- --------------------------------------------------------
 
@@ -264,11 +265,6 @@ CREATE TABLE `detalle_ingreso` (
   `cantidad` int(11) NOT NULL,
   `precio` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `detalle_ingreso`
---
-
 
 -- --------------------------------------------------------
 
@@ -289,6 +285,25 @@ CREATE TABLE `detalle_venta` (
 -- Volcado de datos para la tabla `detalle_venta`
 --
 
+INSERT INTO `detalle_venta` (`id_detalle_venta`, `Venta_id_venta`, `Articulo_id_articulo`, `cantidad`, `precio`, `metodo_pago`) VALUES
+(36, 27, 5, 3, 3200.00, 1),
+(37, 28, 6, 5, 2500.00, 1),
+(38, 29, 5, 2, 3200.00, 1),
+(39, 30, 6, 2, 2500.00, 1),
+(40, 32, 5, 2, 3200.00, 1),
+(41, 33, 115, 1, 2200.00, 1),
+(42, 34, 120, 1, 3000.00, 1),
+(43, 35, 6, 2, 2500.00, 1),
+(44, 36, 53, 2, 2200.00, 1),
+(45, 37, 6, 2, 2500.00, 1),
+(46, 40, 5, 1, 3200.00, 1),
+(47, 44, 5, 2, 3200.00, 1),
+(48, 45, 5, 2, 3200.00, 1),
+(49, 49, 6, 2, 2500.00, 1),
+(50, 50, 53, 2, 2200.00, 1),
+(51, 51, 5, 1, 3200.00, 1),
+(52, 52, 53, 2, 2200.00, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -308,6 +323,67 @@ CREATE TABLE `gastos` (
 -- Volcado de datos para la tabla `gastos`
 --
 
+INSERT INTO `gastos` (`id_gasto`, `descripcion`, `total`, `id_usuario`, `fecha`, `categoria_gasto`) VALUES
+(30, 'test', 12000.00, 5, '2023-12-09 10:02:07', 9),
+(31, 'test', 5000.00, 5, '2023-12-09 10:02:21', 9),
+(32, 'test', 5000.00, 4, '2023-12-09 18:47:59', 2),
+(33, 'test', 5000.00, 5, '2023-12-09 09:27:38', 2),
+(34, 'test', 10000.00, 4, '2023-12-09 15:37:10', 2),
+(35, 'test', 50000.00, 5, '2023-12-10 15:39:33', 2),
+(36, 'test', 1530000.00, 5, '2023-12-09 17:04:01', 1),
+(37, 'test', 356000.00, 4, '2023-12-09 17:06:38', 11),
+(38, 'test', 200000.00, 5, '2023-12-09 17:09:35', 4),
+(39, 'test', 100000.00, 5, '2023-12-09 17:11:00', 11);
+
+--
+-- Disparadores `gastos`
+--
+DELIMITER $$
+CREATE TRIGGER `gastos` AFTER INSERT ON `gastos` FOR EACH ROW BEGIN
+    DECLARE total_count INT;
+    DECLARE prev_count INT;
+    DECLARE prev_value DECIMAL;
+
+    -- Count records with the same date as NEW.fecha
+    SELECT COUNT(*) INTO total_count FROM totalizados WHERE DATE(fecha) = DATE(NEW.fecha);
+
+    -- Count records with the date one day before NEW.fecha
+    SELECT COUNT(*), COALESCE(SUM(saldo_disponible_acum), 0) INTO prev_count, prev_value
+    FROM totalizados
+    WHERE DATE(fecha) = DATE(DATE_SUB(NEW.fecha, INTERVAL 1 DAY));
+
+    IF total_count > 0 THEN
+        IF NEW.categoria_gasto = 11 THEN
+            UPDATE totalizados
+            SET sum_disponible_diario = total_ingresos - total_operacionales - (total_no_operacionales + NEW.total), saldo_disponible_acum = total_ingresos - total_operacionales - (total_no_operacionales + NEW.total) + 	saldos_anteriores, total_no_operacionales = total_no_operacionales + NEW.total
+            WHERE DATE(fecha) = DATE(NEW.fecha);
+        ELSE
+            UPDATE totalizados
+            SET sum_disponible_diario = total_ingresos - (total_operacionales + NEW.total) -total_no_operacionales, saldo_disponible_acum = total_ingresos - (total_operacionales + NEW.total) -total_no_operacionales + saldos_anteriores ,total_operacionales = total_operacionales + NEW.total
+            WHERE DATE(fecha) = DATE(NEW.fecha);
+        END IF;
+    ELSE
+        IF NEW.categoria_gasto = 11 THEN
+            IF prev_count > 0 THEN
+                INSERT INTO totalizados (fecha, total_no_operacionales, saldos_anteriores, 	sum_disponible_diario, saldo_disponible_acum)
+                VALUES (NEW.fecha, NEW.total, prev_value, - NEW.total, prev_value - NEW.total );
+            ELSE
+                INSERT INTO totalizados (fecha, total_no_operacionales, saldos_anteriores, sum_disponible_diario, saldo_disponible_acum)
+                VALUES (NEW.fecha, NEW.total, 0,  - NEW.total, - NEW.total );
+            END IF;
+        ELSE
+            IF prev_count > 0 THEN
+                INSERT INTO totalizados (fecha, total_operacionales, saldos_anteriores, sum_disponible_diario, saldo_disponible_acum)
+                VALUES (NEW.fecha, NEW.total, prev_value, - NEW.total, prev_value - NEW.total);
+            ELSE
+                INSERT INTO totalizados (fecha, total_operacionales, saldos_anteriores, sum_disponible_diario, saldo_disponible_acum)
+                VALUES (NEW.fecha, NEW.total, 0,  - NEW.total, - NEW.total);
+            END IF;
+        END IF;
+    END IF;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -322,11 +398,6 @@ CREATE TABLE `ingreso` (
   `total` decimal(11,2) NOT NULL,
   `estado` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `ingreso`
---
-
 
 -- --------------------------------------------------------
 
@@ -372,6 +443,31 @@ INSERT INTO `rol` (`id_rol`, `nombre`, `estado`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `totalizados`
+--
+
+CREATE TABLE `totalizados` (
+  `fecha` datetime NOT NULL,
+  `total_ingresos` decimal(11,2) DEFAULT 0.00,
+  `total_operacionales` decimal(11,2) DEFAULT 0.00,
+  `total_no_operacionales` decimal(11,2) DEFAULT 0.00,
+  `saldos_anteriores` decimal(11,2) DEFAULT 0.00,
+  `sum_ganancias` decimal(11,2) DEFAULT 0.00,
+  `sum_disponible_diario` decimal(11,2) DEFAULT 0.00,
+  `saldo_disponible_acum` decimal(11,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `totalizados`
+--
+
+INSERT INTO `totalizados` (`fecha`, `total_ingresos`, `total_operacionales`, `total_no_operacionales`, `saldos_anteriores`, `sum_ganancias`, `sum_disponible_diario`, `saldo_disponible_acum`) VALUES
+('2023-12-09 17:00:47', 6500000.00, 1730000.00, 456000.00, 1250000.00, 650000.00, 4314000.00, 5564000.00),
+('2023-12-10 17:12:10', 4650000.00, 0.00, 0.00, 5564000.00, 465000.00, 4650000.00, 10214000.00);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -391,6 +487,9 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
+INSERT INTO `usuario` (`id_usuario`, `nombres`, `apellidos`, `tipo_documento`, `num_documento`, `email`, `user_password`, `estado`, `rol_id_rol`) VALUES
+(4, 'camilo', 'hernandez', 'cc', '1029232323', 'ch4602117@gmail.com', '12345', 1, 1),
+(5, 'test', 'test', 'cc', '100121212', 'ch4602118@gmail.com', '12345', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -409,6 +508,69 @@ CREATE TABLE `venta` (
 -- Volcado de datos para la tabla `venta`
 --
 
+INSERT INTO `venta` (`id_venta`, `Usuario_id_usuario`, `fecha`, `total`) VALUES
+(27, 5, '2023-12-08 15:34:59', 9600.00),
+(28, 5, '2023-12-08 19:20:25', 12500.00),
+(29, 5, '2023-12-09 08:58:43', 6400.00),
+(30, 5, '2023-12-09 08:59:51', 5000.00),
+(32, 5, '2023-12-09 09:02:21', 6400.00),
+(33, 5, '2023-12-09 09:03:12', 2200.00),
+(34, 5, '2023-12-09 09:05:39', 3000.00),
+(35, 5, '2023-12-09 09:56:18', 5000.00),
+(36, 5, '2023-12-09 09:57:05', 4400.00),
+(37, 5, '2023-12-09 10:02:40', 5000.00),
+(40, 5, '2023-12-09 10:37:18', 3200.00),
+(44, 5, '2023-12-09 10:56:47', 6400.00),
+(45, 5, '2023-12-09 11:01:32', 6400.00),
+(49, 5, '2023-12-09 11:09:08', 5000.00),
+(50, 5, '2023-12-09 11:09:34', 4400.00),
+(51, 5, '2023-12-09 11:23:57', 3200.00),
+(52, 5, '2023-12-09 11:24:28', 4400.00),
+(53, 4, '2023-12-09 17:26:43', 10000.00),
+(54, 5, '2023-12-09 17:27:04', 5000.00),
+(55, 5, '2023-12-09 17:32:44', 10000.00),
+(56, 5, '2023-12-09 17:32:58', 5000.00),
+(62, 4, '2023-12-09 15:57:14', 5000.00),
+(64, 4, '2023-12-09 16:09:02', 10000.00),
+(65, 5, '2023-12-10 16:18:36', 15000.00),
+(66, 4, '2023-12-09 16:19:39', 10000.00),
+(67, 4, '2023-12-09 22:20:34', 5000.00),
+(68, 4, '2023-12-10 16:31:13', 40000.00),
+(69, 4, '2023-12-09 17:00:47', 4500000.00),
+(70, 4, '2023-12-09 23:08:14', 2000000.00),
+(71, 4, '2023-12-10 17:12:10', 4650000.00);
+
+--
+-- Disparadores `venta`
+--
+DELIMITER $$
+CREATE TRIGGER `totalizados_ingresos` AFTER INSERT ON `venta` FOR EACH ROW BEGIN
+DECLARE total_count INT;
+DECLARE prev_value DECIMAL;
+DECLARE prev_count INT;
+SELECT COUNT(*) INTO total_count FROM totalizados WHERE DATE(fecha) = DATE(NEW.fecha);
+
+SELECT COUNT(*), COALESCE(SUM(saldo_disponible_acum), 0) INTO prev_count, prev_value FROM totalizados WHERE DATE(fecha) = DATE(DATE_SUB(NEW.fecha, INTERVAL 1 DAY));
+
+ IF total_count > 0 THEN
+ 	UPDATE totalizados
+ 	SET sum_ganancias = ((total_ingresos + 	NEW.total)*10)/100, sum_disponible_diario = total_ingresos + 	NEW.total - total_operacionales - total_no_operacionales, saldo_disponible_acum = total_ingresos + 	NEW.total - total_operacionales - total_no_operacionales + saldos_anteriores,  total_ingresos = total_ingresos + NEW.total WHERE DATE(fecha) = DATE(NEW.fecha);
+ 
+ ELSE
+ IF prev_count > 0 THEN
+ INSERT INTO totalizados (fecha, total_ingresos, saldos_anteriores, sum_disponible_diario, saldo_disponible_acum, sum_ganancias) VALUES (NEW.fecha, NEW.total, prev_value, NEW.total,NEW.total + prev_value, (NEW.total* 10)/100);
+ 
+ ELSE
+ 
+  INSERT INTO totalizados (fecha, total_ingresos, saldos_anteriores, sum_disponible_diario, saldo_disponible_acum, sum_ganancias) VALUES (NEW.fecha, NEW.total, 0, NEW.total, NEW.total, (NEW.total* 10)/100);
+ 
+ END IF;
+ 	
+ END IF;
+ 
+ END
+$$
+DELIMITER ;
 
 --
 -- Índices para tablas volcadas
@@ -511,7 +673,7 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de la tabla `categoria_gastos`
 --
 ALTER TABLE `categoria_gastos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_ingreso`
@@ -523,13 +685,13 @@ ALTER TABLE `detalle_ingreso`
 -- AUTO_INCREMENT de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
-  MODIFY `id_detalle_venta` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_detalle_venta` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT de la tabla `gastos`
 --
 ALTER TABLE `gastos`
-  MODIFY `id_gasto` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_gasto` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `ingreso`
@@ -553,13 +715,13 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `id_venta` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_venta` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- Restricciones para tablas volcadas
